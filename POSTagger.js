b6dc7668640a1afd3bc15bfe2e1c60ff09dfffb3
Said SAID EL IMAM <saidelimam@gmail.com>
2013-12-01 02:21:28 +0000

    more rules and corrections
    added some more rules and managed exceptions for better POS tagging results.
    the last rule of changing nouns ending with "ing" to gerunnds doesn't work for nouns like (thing, ping, something, nothing...)

--- POSTagger.js
+++ POSTagger.js
@@ -63,46 +63,68 @@ POSTagger.prototype.tag = function(words){
      **/
     for (var i = 0; i < words.length; i++) {
         var word = ret[i];
-	//  rule 1: DT, {VBD | VBP} --> DT, NN
-        if (i > 0 && ret[i - 1] == "DT") {
-            if (word == "VBD" ||
-            word == "VBP" ||
-            word == "VB") {
-                ret[i] = "NN";
-            }
+        //EXCEPTIONS
+        if (i > 0 && words[i] === "'s" && i<words.length-1) {
+            if (/^VBG$/.test(ret[i+1])) ret[i] = 'VBZ';//'s + VBG -> is
+            else ret[i] = 'POS';//'s + NN -> POS
         }
-        // rule 2: convert a noun to a number (CD) if "." appears in the word
-        if (startsWith(word, "N")) {
-			if (words[i].indexOf(".") > -1) {
-              // url if there are two contiguous alpha characters
-              if (/[a-zA-Z]{2}/.test(words[i]))
-                ret[i] = "URL";
-              else
-                ret[i] = "CD";
-            }
-			// Attempt to convert into a number
-            if (parseFloat(words[i]))
-                ret[i] = "CD";
-        }
-        // rule 3: convert a noun to a past participle if words[i] ends with "ed"
-        if (startsWith(ret[i], "N") && endsWith(words[i], "ed")) 
-            ret[i] = "VBN";
-        // rule 4: convert any type to adverb if it ends in "ly";
-        if (endsWith(words[i], "ly")) 
-            ret[i] = "RB";
-        // rule 5: convert a common noun (NN or NNS) to a adjective if it ends with "al"
-        if (startsWith(ret[i], "NN") && endsWith(word, "al")) 
-            ret[i] = i, "JJ";
-        // rule 6: convert a noun to a verb if the preceding work is "would"
-        if (i > 0 && startsWith(ret[i], "NN") && words[i - 1].toLowerCase() == "would")
+        else if (i > 0 && words[i].toLowerCase() == "like" && /^(PRP)$/.test(ret[i-1])) {//like:VB/IN
             ret[i] = "VB";
-        // rule 7: if a word has been categorized as a common noun and it ends with "s",
-        //         then set its type to plural common noun (NNS)
-        if (ret[i] == "NN" && endsWith(words[i], "s")) 
-            ret[i] = "NNS";
-        // rule 8: convert a common noun to a present participle verb (i.e., a gerund)
-        if (startsWith(ret[i], "NN") && endsWith(words[i], "ing"))
-            ret[i] = "VBG";
+        }
+        // rule: A word starting with a capital letter in the middle of the sentence is a NNP
+        else if (i > 0 && /^[A-Z]/.test(words[i]) ) {
+            ret[i] = "NNP";
+        }
+        else {
+            // rule: {DT | JJ}, {VBD | VBP} --> DT|JJ, NN
+            if (i > 0 && (ret[i - 1] == "DT" || startsWith(ret[i - 1],"JJ"))) {
+                if (word == "VBD" ||
+                word == "VBP" ||
+                word == "VB") {
+                    ret[i] = "NN";
+                }
+            }
+            // rule: convert a noun to a number or url (CD|URL) if "." appears in the word
+            if (startsWith(word, "N")) {
+    			if (words[i].indexOf(".") > -1) {
+                  // url if there are two contiguous alpha characters or more after the point
+                  if (/\.[a-zA-Z]{2}/.test(words[i]))
+                    ret[i] = "URL";
+                  else if (/[0-9]*\.[0-9]+/.test(words[i]))
+                    ret[i] = "CD";
+                  else
+                    ret[i] = "NN";
+                }
+    			// Attempt to convert into a number
+                if (parseFloat(words[i]))
+                    ret[i] = "CD";
+            }
+            // rule: convert a noun to a past participle if words[i] ends with "ed" and doesn't begin with a capital case
+            if (startsWith(ret[i], "N") && endsWith(words[i], "ed") && /^[a-z]/.test(words[i])) 
+                ret[i] = "VBN";
+            // rule: convert a noun to a plural noun if words[i] ends with "ses"
+            if (startsWith(ret[i], "NN") && endsWith(words[i], "ses")) 
+                ret[i] = "NNS";
+            // rule: convert any type to adverb if it ends in "ly" and more than 3 letters (!fly);
+            if (endsWith(words[i], "ly") && words[i].length > 3) 
+                ret[i] = "RB";
+            // rule: convert a common noun (NN or NNS) to a adjective if it ends with "al"
+            if (startsWith(ret[i], "NN") && endsWith(word, "al")) 
+                ret[i] = i, "JJ";
+            // rule: convert a noun to a verb if the preceding work is "would"
+            if (i > 0 && startsWith(ret[i], "NN") && words[i - 1].toLowerCase() == "would")
+                ret[i] = "VB";
+            // rule: convert a noun to a verb if the preceding work is a PRP or NN
+            if (i > 0 && startsWith(ret[i], "NN") && (ret[i-1] == "PRP" || startsWith(ret[i-1], "NN")))
+                ret[i] = "VB";
+            // rule: if a word has been categorized as a common noun and it ends with "s",
+            //         then set its type to plural common noun (NNS)
+            if (ret[i] == "NN" && endsWith(words[i], "s") && /[^ous]$/.test(words[i].toLowerCase())) 
+                ret[i] = "NNS";
+            // rule: convert a common noun to a present participle verb (i.e., a gerund)
+            /*if (startsWith(ret[i], "NN") && endsWith(words[i], "ing"))
+                ret[i] = "VBG";*/
+        }
     }
 	var result = new Array();
 	for (i in words) {
